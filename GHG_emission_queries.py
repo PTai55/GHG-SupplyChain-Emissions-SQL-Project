ada82042b42e955ea46d75da5eb917b4923b0548
PTai55 <163356370+PTai55@users.noreply.github.com>
2026-01-29 16:52:49 +0000

    Updated GHG_emission_queries.py to include a sample query that selects the first 5 rows from the dataset and prints them

--- GHG_emission_queries.py
+++ GHG_emission_queries.py
@@ -4,3 +4,6 @@ import pandas as pd
 csv_path = "/workspaces/Global-Debt-SQL-Project/SupplyChainGHGEmissionFactors_v1.3.0_NAICS_byGHG_USD2022.csv"
 
 df = pd.read_csv(csv_path)
+
+print(duckdb.sql("select * from df limit 5").df())
+
